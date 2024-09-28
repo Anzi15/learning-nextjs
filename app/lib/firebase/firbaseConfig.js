9fee2f8e922dfa7c060cf8900918b63c060c5138
anzi15 <djam4343@gmail.com>
2024-09-28 16:07:35 +0500

    asdf

--- app/lib/firebase/firbaseConfig.js
+++ app/lib/firebase/firbaseConfig.js
@@ -8,14 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAUP7Mncn62Xe0WYGC9TgqsYGBmBD3eXQU",
-  authDomain: "al-zehra.firebaseapp.com",
-  databaseURL: "https://al-zehra-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "al-zehra",
-  storageBucket: "al-zehra.appspot.com",
-  messagingSenderId: "409292615047",
-  appId: "1:409292615047:web:a1282da2b9182c2fdbb722",
-  measurementId: "G-MXCEGHNV6P"
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN_1,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,19 +1,43 @@
-import { collection, getDocs } from "firebase/firestore"
+/* eslint-disable react/no-unescaped-entities */
+
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../lib/firebase/firbaseConfig";
 
-export default async function Page() {
-    const docs = getDocs(collection(db,"Products"))
-    const products = [];
-    const docData = (await docs).forEach((doc)=>{
-        products.push(doc.data())
-    })
-    console.log(products)
+export async function generateMetadata() {
+  const docs = await getDocs(collection(db, "Products"));
+  const products = [];
+  
+  docs.forEach((doc) => {
+    products.push(doc.data());
+  });
 
-    return (
+  const productCount = products.length;
+  const title = productCount > 0 
+    ? `We have ${productCount} product${productCount > 1 ? 's' : ''} available!` 
+    : "No products available";
+
+  return {
+    title,
+    description: "Explore our range of products.",
+  };
+}
+
+export default async function ProductsPage() {
+  const docs = await getDocs(collection(db, "Products"));
+  const products = [];
+
+  docs.forEach((doc) => {
+    products.push(doc.data());
+  });
+
+  return (
+    <>
+      <h1>Products</h1>
       <ul>
         {products.map((product) => (
           <li key={product.title}>{product.title}</li>
         ))}
       </ul>
-    )
-  }
+    </>
+  );
+}
